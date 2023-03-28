770c9af5733ae75495714c57a83a10c0a0f09992
henriqerocha <44968212+henriqerocha@users.noreply.github.com>
2023-03-28 18:42:40 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,47 @@
-function criarCaixaDeTexto() {
+function criarCaixaProventos() {
     // cria o elemento de entrada de texto
-    var caixaDeTexto = document.createElement("input");
-    caixaDeTexto.type = "text";
-    caixaDeTexto.id = "meu-texto";
-    caixaDeTexto.name = "meu-texto";
+    var caixaDescricao = document.createElement("input");
+    caixaDescricao.style.backgroundColor = '#d2ffd2';
+    caixaDescricao.type = "text";
+    caixaDescricao.id = "meu-texto";
+    caixaDescricao.name = "descricao";
+
+    var caixaValor = document.createElement("input");
+    caixaValor.style.backgroundColor = '#d2ffd2';
+    caixaValor.type = "number";
+    caixaValor.id = "meu-texto";
+    caixaValor.name = "descricao";
 
 
 
     // adiciona a caixa de texto à div
     var div = document.getElementById("descricao");
-    div.appendChild(caixaDeTexto);
+    div.appendChild(caixaDescricao);
+
+    var div = document.getElementById("valor");
+    div.appendChild(caixaValor);
+  }
+
+  function criarCaixaDespesas() {
+    // cria o elemento de entrada de texto
+    var caixaDescricao = document.createElement("input");
+    caixaDescricao.style.backgroundColor = '#ffd2d2';
+    caixaDescricao.type = "text";
+    caixaDescricao.id = "meu-texto";
+    caixaDescricao.name = "descricao";
+
+    var caixaValor = document.createElement("input");
+    caixaValor.style.backgroundColor = '#ffd2d2';
+    caixaValor.type = "number";
+    caixaValor.id = "meu-texto";
+    caixaValor.name = "descricao";
+
+
+
+    // adiciona a caixa de texto à div
+    var div = document.getElementById("descricao");
+    div.appendChild(caixaDescricao);
+
+    var div = document.getElementById("valor");
+    div.appendChild(caixaValor);
   }
